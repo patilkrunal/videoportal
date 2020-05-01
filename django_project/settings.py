43e4f2e46e5b8249b64dbe026e2f22c3cf31655e
gitkp11 <patilrunal1198@gmail.com>
2020-05-01 16:22:19 +0530

    Initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['kpvideoportal.herokuapp.com']
 
 
 # Application definition
@@ -126,3 +126,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 django_heroku.settings(locals())
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
